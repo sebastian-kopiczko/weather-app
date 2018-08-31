a7216d4dcecee40b1850614272cb14edc8f26fe2
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2018-08-31 12:04:47 +0200

    new html using materialize

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,6 +1,6 @@
 export class Weather{
   constructor(lat, long, lang){
-    this.apiKey = '';
+    this.apiKey = '055a53f5a6b68703fe86456a74f6da17';
     this.lat = lat;
     this.long = long;
     this.lang = 'pl';
@@ -11,6 +11,7 @@ export class Weather{
     const corsEnableUrl = 'https://cors-anywhere.herokuapp.com/';
     const response = await fetch(`${corsEnableUrl}https://api.darksky.net/forecast/${this.apiKey}/${this.lat},${this.long}?lang=${this.lang}&units=auto`);
     const responseData = await response.json();
+    console.log(responseData);
     return responseData;
   }
 
