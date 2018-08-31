11fb7cc4ec818b64eae27bcaa340a72d38d69f5a
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2018-08-31 11:05:30 +0200

    install materialize css via npm, add css and style loader to webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,12 @@ module.exports = {
     filename: 'app.bundle.js',
   },
   module: {
-    rules: [{
+    rules: [
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
         test: /\.js?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
